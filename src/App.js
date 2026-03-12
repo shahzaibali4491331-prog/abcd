7ebf2a67ad2205f9cc2ea19bdf5b18c747bfd7e1
shahzaib <shahzaibdon43210@gmail.com>
2026-03-12 15:17:25 +0500

    shahzaib ali

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ShahzaibAli from './components/ShahzaibAli';
+import Asim from './components/Asim';
 
 function App() {
   return (
     <div className="App">
      <ShahzaibAli/>
+     <Asim/>
     </div>
   );
 }
--- src/components/ShahzaibAli.jsx
+++ src/components/ShahzaibAli.jsx
@@ -4,6 +4,8 @@ const ShahzaibAli = () => {
   return (
     <div>
       my name is shahzaib and father  name is Arif
+      <div>New repo </div>
+      <h1> work test </h1>
     </div>
   )
 }
