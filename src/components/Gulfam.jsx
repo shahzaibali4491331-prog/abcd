7a7fec3f4a2d912ea2b2edd004d6048ee9d7c9df
Rana gulfam <gulfamelahi7@gmail.com>
2026-03-12 15:25:41 +0500

    gulfam send code

---
+++ src/components/Gulfam.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Gulfam = () => {
+  return (
+    <div>
+        <h1>my current state is </h1>
+        <button type='button'>click me</button>
+    </div>
+  )
+}
+
+export default Gulfam
