b288f4bba4791fa2b9fc0de21684c57989cc0f90
shahzaib <shahzaibdon43210@gmail.com>
2026-03-12 15:05:20 +0500

    file name is github

---
+++ src/components/ShahzaibAli.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShahzaibAli = () => {
+  return (
+    <div>
+      my name is shahzaib and father  name is Arif
+    </div>
+  )
+}
+
+export default ShahzaibAli
